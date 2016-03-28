d976f90377f30a85a5ea5daf5075437d29675f8a
daysgone <xmtbikerx@hotmail.com>
2016-03-28 01:31:31 -0400

    small changes

--- scanner/Hardware.py
+++ scanner/Hardware.py
@@ -4,41 +4,58 @@ import time
 
 TEST = False
 
-
 class LED(object):
     """
-
+    should set these up to use PWM
     """
-    def __init__(self):
+    def __init__(self, pin=7):
+        GPIO.setmode(GPIO.BOARD)  # physical pin numbers
+
         # pin values most likely will be changed
-        self.r = 11
-        self.g = 13
-        self.b = 15
-        self.colors = [self.r, self.g, self.b]
-        GPIO.setmode(GPIO.BOARD)
+        self.pin = pin
+        GPIO.setup(pin, GPIO.OUT)
+
+    def on(self, val):
+        GPIO.output(self.pin, val)
+
+    def off(self):
+        GPIO.output(self.pin, 0)
+
+    @staticmethod
+    def cleanup():
+        # this must be called or lights will stay on after code is executed
+        GPIO.cleanup()
+
+class RGB_LED(LED):
+    def __init__(self, pin_r=11, pin_g=13, pin_b=15):
+        super(RGB_LED,self).__init__(pin_r)
+
+        # pin values most likely will be changed
+        self.red = self.pin
+        self.green = pin_g
+        self.blue = pin_b
+        self.colors = [self.red, self.green, self.blue]
 
         # setup the pins to output
         for pin in self.colors:
             GPIO.setup(pin, GPIO.OUT)
-            GPIO.output(pin, 0)
+            # GPIO.output(pin, 0)
 
-    def ON(self, color): #, color=[1, 1, 1]):
-        print len(self.colors)
-        for c in len(self.colors):
-            print c
+    def on(self, color): #, color=[1, 1, 1]):
+        for i, pin in enumerate(self.colors):
+             GPIO.output(pin, color[i])
 
-        print 'out of for loop'
-        return False
-
-    def OFF(self):
+    def off(self):
         for pin in self.colors:
-             GPIO.output(pin,0)
+             GPIO.output(pin, 0)
 
-    def cleanup(self):
-        GPIO.cleanup()
+
+class Buzzer(object):
+    pass
+
 
 if TEST:
-    led = LED()
+    led = RGB_LED()
     led.ON([0,1,0])
 
     time.sleep(3)
